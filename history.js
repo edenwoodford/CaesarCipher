50c1565dfab9780ae97e4713a1bc5d64c05699d0
Eden Woodford <edenw1@muskingum.edu>
2023-09-07 12:59:59 -0400

    history.js troubleshooted

--- history.js
+++ history.js
@@ -1,8 +1,23 @@
 import { StyleSheet } from './style.js';
-import { Text, View, Image, Button, TextInput } from 'react-native';
+import { Text, View, Image,FlatList, Button, TextInput } from 'react-native';
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-export default function History() {
-
+export default function History({route}) {
+    return (
+        <View style={styles.container}>
+          <Text>History Screen</Text>
+          <FlatList
+            data={route.params.answers}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) => (
+              <View>
+                <Text>Original Message: {item.originalMessage}</Text>
+                <Text>Encryption Key: {item.encryptionKey}</Text>
+                <Text>Result: {item.result}</Text>
+              </View>
+            )}
+          />
+        </View>
+      );
 }
