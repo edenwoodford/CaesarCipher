8ff021036c5ea797604894183adc6da2591732a5
Eden Woodford <edenw1@muskingum.edu>
2023-09-08 12:59:21 -0400

    persistent store

--- App.js
+++ App.js
@@ -6,28 +6,34 @@ import CaeserCipher from './CaeserCipher.js';
 import History from './history.js';
 import { store } from './store.js';
 import {Provider} from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+import { ActivityIndicator } from 'react-native';
+
+const persistor = persistStore(store)
 const Stack = createNativeStackNavigator();
 
 
 export default function App() {
 
   return (
-    <Provider store = {store} >
-
-  <NavigationContainer>
-  <Stack.Navigator>
-  <Stack.Screen
-  name="Home"
-  component={CaeserCipher}
-  options={{title: 'Page 1'}}
-  />
-  <Stack.Screen
-  name="History"
-  component={History}
-  options={{title: 'Page 2'}}
-  />
-  </Stack.Navigator>
-  </NavigationContainer>
-  </Provider>
+    <Provider store={store}>
+      <PersistGate loading={<ActivityIndicator />} persistor={persistor}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={CaeserCipher}
+              options={{ title: 'Page 1' }}
+            />
+            <Stack.Screen
+              name="History"
+              component={History}
+              options={{ title: 'Page 2' }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
-  }
+}
--- store.js
+++ store.js
@@ -1,9 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit'
 import statsReducer from './model.js'
 import historyReducer from './model2.js'
-export const store = configureStore({
-reducer: {
+import {combineReducers} from "redux"; 
+import { persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, }
+from 'redux-persist'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+const reducers = combineReducers({
     stats: statsReducer,
-    history: historyReducer
-}
-})
+    history: historyReducer,
+  });
+  
+  const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+  };
+  
+  const persistedReducer = persistReducer(persistConfig, reducers);
+  
+  export const store = configureStore({
+    reducer: persistedReducer,
+    devTools: true,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
+  });
