6a26e7eed05860fb00243284b5d68fa5f3bc2cc8
Eden Woodford <eden.woodford@gmail.com>
2023-09-19 10:39:18 -0400

    start tracking

--- MessageDetail.js
+++ MessageDetail.js
@@ -1,45 +1,59 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, Button, Alert } from 'react-native';
 import { useDispatch } from 'react-redux';
-import {deleteHistory} from './model2';
-import { AntDesign } from '@expo/vector-icons';
+import { deleteHistory } from './model2';
 import * as Location from 'expo-location';
 
-
-export default function MessageDetail({route,navigation }) {
-  const {message} = route.params;
+export default function MessageDetail({ route, navigation }) {
+  const { message } = route.params;
   const dispatch = useDispatch();
   const goBack = () => {
     navigation.navigate('History');
   };
-  const [location, setLocation] = useState(null)
-  const [messages, setMessage] = useState('Waiting...')
+  const [location, setLocation] = useState(null);
+  const [subscription, setSubscription] = useState(null);
+  const [messages, setMessage] = useState('Waiting...');
+  
+  const watchLocation = (location) => {
+    setMessage(JSON.stringify(location));
+  };
 
+  const startTracking = async () => {
+    result = await Location.watchPositionAsync({
+      accuracy: Location.Accuracy.Highest,
+      distanceInterval: 3,
+    }, watchLocation);
+    setSubscription(result);
+  };
 
-    useEffect(() => {
+  const stopTracking = async () => {
+    await subscription.remove();
+    setMessage("Ready for tracking");
+  };
+
+  useEffect(() => {
     (async () => {
-    let { status } = await Location.requestForegroundPermissionsAsync()
-    if (status !== 'granted') {
-    setMessage('Permission to access location was denied')
-    return
-    }
-    let location = await Location.getCurrentPositionAsync({accuracy:
-    Location.Accuracy.Highest})
-    setLocation(location)
-    setMessage(JSON.stringify(location))
-    })()
-    }, [] );
-
+      let fg = await Location.requestForegroundPermissionsAsync();
+      if (fg.status !== 'granted') {
+        setMessage('Permission to access foreground location was denied');
+        return;
+      }
+      setMessage("Ready for tracking");
+    })();
+  }, []); 
 
   return (
-    <View>
-      <Text>Original Message: {message.originalMessage}</Text>
-      <Text>Encryption Key: {message.encryptionKey}</Text>
-      <Text>Result: {message.result}</Text>
-    <Text>{messages}</Text>
-     <AntDesign.Button name="banckward"  > Decrypt
-  </AntDesign.Button>
+    <View style={{ flexDirection: 'column' }}>
+      <View>
+        <Text>Original Message: {message.originalMessage}</Text>
+        <Text>Encryption Key: {message.encryptionKey}</Text>
+        <Text>Result: {message.result}</Text>
+        <Text>{messages}</Text>
+      </View>
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <Button title="Decrypt" />
+        <Button title="Start Tracking" onPress={startTracking} />
+      </View>
     </View>
   );
-  
-  }
+}
